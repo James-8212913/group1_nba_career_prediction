e5d8ebdef4b0bb4408ed07e134abf59ba1601331
James M <james.w.murray@student.uts.edu.au>
2022-02-14 19:58:10 +1100

    First commit

---
+++ https:/github.com/James-8212913/group1_nba_career_prediction.git/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Public project to predict the career progression and length of nba players based on playing stats',
+    author='group_1',
+    license='MIT',
+)
